7b5e746cc1800825d12c34a07b8d5cdcb2d95313
Caleb Smith <karagenit@outlook.com>
2017-08-07 03:57:44 +0000

    Add Basic Script

---
+++ sloc.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require "octokit"
+require "net/http"
+require "uri"
+
+print "Username: "
+uname = gets.chomp
+print "Password: "
+pword = gets.chomp
+
+octoclient = Octokit::Client.new(:login => uname, :password => pword)
+
+token = octoclient.create_authorization(:scopes => ["public_repo", "user"], :note => "SLOC Script").attrs[:token]
+
+query = Hash.new
+query['query'] = IO.read('query.json')
+query = query.to_json
+
+uri = URI.parse("https://api.github.com/graphql")
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+request = Net::HTTP::Post.new(uri)
+request['Authorization'] = "bearer #{token}"
+request['Content-type'] = "application/json"
+request.body = query
+
+repos = JSON.parse(http.request(request).body)
+
+puts repos.inspect
